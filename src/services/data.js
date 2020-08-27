77981274924da6094ee44a5f59a0f5f7b47b304a
Cody Brown <cody6lefever@gmail.com>
2020-08-27 16:20:12 -0700

    began adding data

---
+++ src/services/data.js
@@ -0,0 +1,46 @@
+export const data = {
+  firstName: 'Cody',
+  middleName: 'Lyle',
+  lastName: 'Brown',
+  maidenName: 'Lefever',
+  profilePic: 'pic stored locally',
+  email: 'clb2934@gmail.com',
+  website: 'codylylebrown.com',
+  github: 'email',
+  linkedin: 'email',
+  skills: [ 
+    'javascript', 
+    'react',
+    'HTML/CSS' 
+  ],
+  projects: [
+    {
+      title: 'Shadespeare',
+      imgUrl: 'pic stored locally',
+      websiteUrl: 'shadespeare.com',
+      description: 'bard',
+      technology: 'the, tech, we, used',
+      githubUrl: 'url'
+    },
+    {
+      title: 'Mosaic',
+      imgUrl: 'pic stored locally',
+      websiteUrl: 'shadespeare.com',
+      description: 'bard',
+      technology: 'the, tech, we, used',
+      githubUrl: 'url'
+    }
+  ],
+  hackathons: [
+    {
+      title: 'AWS',
+      challenge: 'aws',
+      result: '3rd place'
+    },
+    {
+      title: 'TAO',
+      challenge: 'BLE Relay',
+      result: 'honorable mention'
+    }
+  ]
+};
